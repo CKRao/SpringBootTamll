3734f4c36b6181c0ee78cb961bf90caf6cc5af2d
“C <clarkrao25@gmail.com>
2018-11-14 23:30:37 +0800

    分页

--- springboottmall/src/main/java/com/clarkrao/springboottmall/controller/CategoryController.java
+++ springboottmall/src/main/java/com/clarkrao/springboottmall/controller/CategoryController.java
@@ -2,8 +2,10 @@ package com.clarkrao.springboottmall.controller;
 
 import com.clarkrao.springboottmall.pojo.Category;
 import com.clarkrao.springboottmall.service.CategoryService;
+import com.clarkrao.springboottmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,7 +22,11 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping(value = "/categories")
-    public List<Category> list() throws Exception{
-        return categoryService.list();
+    public Page4Navigator<Category> list(@RequestParam(value = "start", defaultValue = "0")
+        int start, @RequestParam(value = "size", defaultValue = "5") int size) throws Exception{
+        start = start < 0 ? 0 : start;
+        //5表示导航分页最多有5个，像 [1,2,3,4,5] 这样
+        Page4Navigator<Category> page = categoryService.list(start, size, 5);
+        return page;
     }
 }
--- springboottmall/src/main/java/com/clarkrao/springboottmall/service/CategoryService.java
+++ springboottmall/src/main/java/com/clarkrao/springboottmall/service/CategoryService.java
@@ -2,7 +2,11 @@ package com.clarkrao.springboottmall.service;
 
 import com.clarkrao.springboottmall.dao.CategoryDAO;
 import com.clarkrao.springboottmall.pojo.Category;
+import com.clarkrao.springboottmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +21,15 @@ import java.util.List;
 public class CategoryService {
     @Autowired
     private CategoryDAO categoryDAO;
+
+    public Page4Navigator<Category> list(int start,int size,int navigatePages){
+        Page4Navigator<Category> page4Navigator;
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        Pageable pageable = new PageRequest(start, size, sort);
+        Page pageFromJPA = categoryDAO.findAll(pageable);
+        page4Navigator = new Page4Navigator<>(pageFromJPA, navigatePages);
+        return page4Navigator;
+    }
 
     public List<Category> list(){
         Sort sort = new Sort(Sort.Direction.DESC, "id");
